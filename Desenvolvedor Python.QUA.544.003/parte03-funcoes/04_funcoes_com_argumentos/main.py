dcf23f4c14d001fe3bf9988656f8dc6d20412137
jorgefrausino <jorge.frausino@aluno.senai.br>
2026-08-13 21:30:51 -0300

    Aula de hoje 13/08/26.

---
+++ Desenvolvedor Python.QUA.544.003/parte03-funcoes/04_funcoes_com_argumentos/main.py
@@ -0,0 +1,38 @@
+import math
+import os 
+
+def limpar():
+    os.system("cls"if os.name == "nt" else "clear")
+
+def area_quadrilatero(b, h):
+        return b*h
+
+def area_triangulo(b, h):
+        return (b*h)/2
+
+def area_circulo(r):
+    return math.pi*(r**2)
+# algoritmo principal
+limpar()
+
+while True:
+    print("1 - Calcular área do quadrilátero.")
+    print("2 - Calcular área do triangulo.")
+    print("2 - Calcular área do circulo.")
+    print("4 - Sair do programa.")
+    opcao = input("Informe a opção desejada: ").strip()
+    limpar()
+    match opcao:
+          case"1":
+                b = float(input("Informe o valor da base: ").replace(",","."))
+                h = float(input("Informe o valor da altura: ").replace(",","."))
+                print(f"Área do quadrilátero é {area_quadrilatero(b, h)}.")
+                continue
+          case "2":
+                b = float(input("Informe o valor da base: ").replace(",","."))
+                h = float(input("Informe o valor da altura: ").replace(",","."))
+                print(f"Área do triangulo é {area_triangulo(b, h)}.")
+                continue
+          case "3":
+                r = float(input("Informe o valor do raio: ").replace(",","."))
+                
---
+++ Desenvolvedor Python.QUA.544.003/parte03-funcoes/05_modulo/main.py
@@ -0,0 +1,9 @@
+import modulo
+
+
+modulo.limpar()
+
+a = float(input("Informe o valor de 'a': ").replace(",","."))
+b = float(input("Informe o valor de 'b': ").replace(",","."))
+x = modulo.limpar()
+print(F"Valor de x é (x).")
