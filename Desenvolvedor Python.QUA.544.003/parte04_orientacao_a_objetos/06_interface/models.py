e6da20bb8f9a11e5c882f2bb2150ca4be5e37956
jorgefrausino <jorge.frausino@aluno.senai.br>
2026-08-24 21:39:24 -0300

    Aula do dia 24/08/2026.

---
+++ Desenvolvedor Python.QUA.544.003/parte04_orientacao_a_objetos/06_interface/models.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+
+class IConta(ABC):
+    @abstractmethod
+    def consultar_conta():
+        pass
+
+    @abstractmethod
+    def fazer_deposito(valor):
+        pass
+
+    @abstractmethod
+    def fazer_saque(valor):
+        pass
+
+    def Conta(IConta):
+        def __init__(self,titular,cpf,agencia,n_conta,saldo):
+            self.__titular + titular
+            self.__cpf = cpf
+            self.__n_conta = n_conta
+            self.__saldo = saldo
+
+            @property
+            def titular(self):
+                return self.__titular
+
+            @titular.setter
+            def titular(self,titular):
+                self.__titular = titular
+                
+            @property
+            def cpf(self):
+                return self.cpf
+
+            @titular.setter
+            def cpf(self,cpf):
+                self.cpf = cpf
+            @property
+            def n_conta(self):
+                return self.n_conta
+
+            @titular.setter
+            def n_conta(self,n_conta):
+                self.__n_conta = n_conta
+           
+            @property
+            def saldo(self):
+                return self.saldo
+
+            @titular.setter
+            def saldo(self,saldo):
+                self.saldo = saldo
+
+            # métodos da interface
+            def consultar_conta(self):
+                print(f"Nome do titular da conta: {self.__titular}")
+                print(f"CPF do titular da conta: {self.__cpf}")
+                print(f"Agência da conta: {self.__agencia}")
+                print(f"Número da conta: {self.__n_conta}")
+                print(f"Saldo da conta: R$ {self.__saldo:.2f}")
+            def fazer_deposito(self,valor):
+                self.__saldo += valor
+                return self.__saldo
+
+            def fazer_saque(self,valor):
+                self.__saldo -= valor
+                return self.__saldo    
+           
